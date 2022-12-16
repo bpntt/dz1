ba49c064d4406bf4554661ad73862a45b4637a9b
BPNTT <haataya@163.com>
2022-12-16 22:37:32 +0600

    dz1

---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write your number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write your number 2");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{  
+Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
